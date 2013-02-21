0ecdc7cf050286c09fc4c39376602a34a1db7efc
abestel <adrien.bestel@gmail.com>
2013-02-21 18:54:00 +0100

    Ajout règles (test)

--- application/views/welcome_index.php
+++ application/views/welcome_index.php
@@ -2,7 +2,7 @@
 <html lang="fr">
   <head>
     <meta charset="utf-8">
-    <title>Killer -by SPIPS</title>
+    <title>Killer - by SPIPS</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="SPIPS Polytech Paris-Sud">
